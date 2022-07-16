fb45bf0b1206a7b79782f1661e3715a4ce05011d
JAVAKOR <wldyd5689@gmail.com>
2022-07-16 21:00:25 +0900

    Zadanie próbne 2

---
+++ STUDY/src/boj/BOJ8871.java
@@ -0,0 +1,14 @@
+package boj;
+import java.util.*;
+
+public class BOJ8871 { // Zadanie próbne 2
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.print((n + 1) * 2 + " " + (n + 1) * 3);
+        sc.close();
+    }
+}
