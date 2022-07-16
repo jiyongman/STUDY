b703fad20604b5c7d550beec8e49a53179c7cb68
JAVAKOR <wldyd5689@gmail.com>
2022-07-16 18:33:18 +0900

    참/거짓이 서로 같을 때에만 참 출력하기

---
+++ STUDY/src/codeup/CODEUP1057.java
@@ -0,0 +1,20 @@
+package codeup;
+import java.util.*;
+
+public class CODEUP1057 { // 참/거짓이 서로 같을 때에만 참 출력하기
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a == b) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+
+        sc.close();
+    }
+}
