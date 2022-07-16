4a9e6b0ce661b20369066e11a46a67a5b2a32536
JAVAKOR <wldyd5689@gmail.com>
2022-07-16 17:54:02 +0900

    참 거짓 바꾸기

---
+++ STUDY/src/codeup/CODEUP1053.java
@@ -0,0 +1,18 @@
+package codeup;
+import java.util.*;
+
+public class CODEUP1053 { // 참 거짓 바꾸기
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (a == 1) {
+            System.out.println(0);
+        } else {
+            System.out.println(1);
+        }
+
+        sc.close();
+    }
+}
