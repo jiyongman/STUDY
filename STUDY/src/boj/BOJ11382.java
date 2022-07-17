a1cbc7c47238746e507b012373b7516fef9f5252
JAVAKOR <wldyd5689@gmail.com>
2022-07-17 11:23:42 +0900

    꼬마 정민

---
+++ STUDY/src/boj/BOJ11382.java
@@ -0,0 +1,16 @@
+package boj;
+import java.util.*;
+
+public class BOJ11382 { // 꼬마 정민
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+        long C = sc.nextLong();
+
+        System.out.println(A + B + C);
+        sc.close();
+    }
+}
