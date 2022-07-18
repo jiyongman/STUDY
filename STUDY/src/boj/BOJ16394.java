cf4e0193867fd3c2d6b57248ff1792160411674d
JAVAKOR <wldyd5689@gmail.com>
2022-07-18 12:20:38 +0900

    홍익대학교

---
+++ STUDY/src/boj/BOJ16394.java
@@ -0,0 +1,14 @@
+package boj;
+import java.util.*;
+
+public class BOJ16394 { // 홍익대학교
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(); // 특정 년도
+
+        System.out.println(N - 1946); // 홍익대학교 개교 주년
+        sc.close();
+    }
+}
