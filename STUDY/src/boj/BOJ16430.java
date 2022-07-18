384c4b35c5f34f4d9931e7131cd54d1f7a1935cb
JAVAKOR <wldyd5689@gmail.com>
2022-07-18 12:24:42 +0900

    제리와 톰

---
+++ STUDY/src/boj/BOJ16430.java
@@ -0,0 +1,15 @@
+package boj;
+import java.util.*;
+
+public class BOJ16430 { // 제리와 톰
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(B - A + " " + B);
+        sc.close();
+    }
+}
