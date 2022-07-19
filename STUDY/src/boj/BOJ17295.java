66c97e9d6638af6ccb6f85213f508931e247eff2
JAVAKOR <wldyd5689@gmail.com>
2022-07-19 11:27:00 +0900

    엔드게임 스포일러

---
+++ STUDY/src/boj/BOJ17295.java
@@ -0,0 +1,14 @@
+package boj;
+import java.util.*;
+
+public class BOJ17295 { // 엔드게임 스포일러
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        System.out.println("Avengers: Endgame");
+        sc.close();
+    }
+}
