ba308948bf0c10d55b1cdb7c39a714265029f63f
JAVAKOR <wldyd5689@gmail.com>
2022-07-19 11:33:10 +0900

    Арифметическая магия

---
+++ STUDY/src/boj/BOJ18096.java
@@ -0,0 +1,14 @@
+package boj;
+import java.util.*;
+
+public class BOJ18096 { // Арифметическая магия
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        System.out.println("1");
+        sc.close();
+    }
+}
