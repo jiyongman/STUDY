80f0ca49253156a8eeae274d69ab95831bdf8a03
JAVAKOR <wldyd5689@gmail.com>
2022-07-19 13:08:27 +0900

    Shares

---
+++ STUDY/src/boj/BOJ3733.java
@@ -0,0 +1,18 @@
+package boj;
+import java.util.*;
+
+public class BOJ3733 { // Shares
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) { // End Of File 처리
+            int N = sc.nextInt();
+            int S = sc.nextInt();
+            System.out.println(S / (N + 1));
+        }
+
+        sc.close();
+    }
+}
