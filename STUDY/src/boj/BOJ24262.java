579a4ea6d3df37e2d21b43035216c47e946c4b65
JAVAKOR <wldyd5689@gmail.com>
2022-07-20 23:19:28 +0900

    알고리즘 수업 - 알고리즘의 수행 시간 1

---
+++ STUDY/src/boj/BOJ24262.java
@@ -0,0 +1,15 @@
+package boj;
+import java.util.*;
+
+public class BOJ24262 { // 알고리즘 수업 - 알고리즘의 수행 시간 1
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(1);
+        System.out.println(0); // 어떤 n이 들어와도 항상 상수시간이 소요
+        sc.close();
+    }
+}
