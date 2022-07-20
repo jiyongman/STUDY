fc1f536a849d9fb35a70e152fc81b08991383774
JAVAKOR <wldyd5689@gmail.com>
2022-07-20 23:10:35 +0900

    身長 (Height)

---
+++ STUDY/src/boj/BOJ24086.java
@@ -0,0 +1,15 @@
+package boj;
+import java.util.*;
+
+public class BOJ24086 { // 身長 (Height)
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(B - A);
+        sc.close();
+    }
+}
