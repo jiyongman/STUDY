15b5cd2b36152cb08170e9a1e91645522614ee1e
JAVAKOR <wldyd5689@gmail.com>
2022-07-20 21:44:27 +0900

    余り (Remainder)

---
+++ STUDY/src/boj/BOJ24078.java
@@ -0,0 +1,14 @@
+package boj;
+import java.util.*;
+
+public class BOJ24078 { // 余り (Remainder)
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+
+        System.out.println(X % 21);
+        sc.close();
+    }
+}
