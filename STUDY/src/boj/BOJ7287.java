a5fd8b7c1831d0e73b13cfb6362a84af15371d9b
jiyongman <87258054+jiyongman@users.noreply.github.com>
2022-07-23 11:19:19 +0900

    Update BOJ7287.java

--- STUDY/src/boj/BOJ7287.java
+++ STUDY/src/boj/BOJ7287.java
@@ -5,6 +5,6 @@ public class BOJ7287 { // 등록
     public static void main(String[] args) {
 
         System.out.println("122"); // 맞은 문제의 수
-        System.out.println("jiyong") // 아이디
+        System.out.println("jiyong"); // 아이디
     }
 }
