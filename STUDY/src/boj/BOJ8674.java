be39db926670e2328696902117eae55e3dba24d7
JAVAKOR <wldyd5689@gmail.com>
2022-07-24 14:55:19 +0900

    Tabliczka

--- STUDY/src/boj/BOJ8674.java
+++ STUDY/src/boj/BOJ8674.java
@@ -6,8 +6,8 @@ public class BOJ8674 { // Tabliczka
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        long a = sc.nextLong(); // the width size
-        long b = sc.nextLong(); // the length size
+        long a = sc.nextLong(); // the size of width
+        long b = sc.nextLong(); // the size of length
 
         if (a % 2 != 0 && b % 2 != 0) {
             System.out.println(Math.min(a, b));
