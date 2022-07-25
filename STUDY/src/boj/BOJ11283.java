47e4e7f0dd13e0f4bb07108df7e9dd8adfda01ec
JAVAKOR <wldyd5689@gmail.com>
2022-07-25 15:24:05 +0900

    한글 2

---
+++ STUDY/src/boj/BOJ11283.java
@@ -0,0 +1,14 @@
+package boj;
+import java.util.*;
+
+public class BOJ11283 { // 한글 2
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+
+        System.out.println((int) ch - 44031);
+        sc.close();
+    }
+}
