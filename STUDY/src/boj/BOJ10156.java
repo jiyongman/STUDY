51b95fee29130df8849ec35ec980510d58c4fa96
JAVAKOR <wldyd5689@gmail.com>
2022-07-25 11:49:13 +0900

    과자

---
+++ STUDY/src/boj/BOJ10156.java
@@ -0,0 +1,21 @@
+package boj;
+import java.util.*;
+
+public class BOJ10156 { // 과자
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int K = sc.nextInt(); // 과자 한 개의 가격
+        int N = sc.nextInt(); // 사려고 하는 과자의 개수
+        int M = sc.nextInt(); // 현재 동수가 가진 돈
+
+        if (K * N > M) {
+            System.out.println(K * N - M);
+        } else {
+            System.out.println(0);
+        }
+
+        sc.close();
+    }
+}
