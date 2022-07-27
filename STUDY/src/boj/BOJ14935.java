74f71839e0d41e9754b4901e420748c447ab2c1c
jiyong <wldyd5689@gmail.com>
2022-07-27 17:33:01 +0900

    FA

---
+++ STUDY/src/boj/BOJ14935.java
@@ -0,0 +1,15 @@
+package boj;
+import java.math.BigInteger;
+import java.util.*;
+
+public class BOJ14935 { // FA
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		BigInteger x = sc.nextBigInteger();
+
+		System.out.println("FA");
+		sc.close();
+	}
+}
