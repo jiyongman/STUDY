eaa98f527fe05584381e27cf2f23b60ea3d487e7
jiyong <wldyd5689@gmail.com>
2022-07-27 17:39:26 +0900

    CAPS

---
+++ STUDY/src/boj/BOJ15000.java
@@ -0,0 +1,14 @@
+package boj;
+import java.util.*;
+
+public class BOJ15000 { // CAPS
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		
+		System.out.println(str.toUpperCase()); // 문자열을 대문자로 변환
+		sc.close();
+	}
+}
