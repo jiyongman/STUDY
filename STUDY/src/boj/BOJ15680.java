f7e9db5bd4be947c2116dbb996c8748dab2c7aa5
jiyong <wldyd5689@gmail.com>
2022-07-29 12:42:39 +0900

    연세대학교

---
+++ STUDY/src/boj/BOJ15680.java
@@ -0,0 +1,19 @@
+package boj;
+import java.util.*;
+
+public class BOJ15680 { // 연세대학교
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		if (N == 0) {
+			System.out.println("YONSEI");
+		} else {
+			System.out.println("Leading the Way to the Future");
+		}
+		
+		sc.close();
+	}
+}
