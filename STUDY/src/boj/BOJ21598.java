79b37732ebe91fb99721fab22cf9d431f5daa0c5
jiyong <wldyd5689@gmail.com>
2022-08-01 11:39:56 +0900

    SciComLove

---
+++ STUDY/src/boj/BOJ21598.java
@@ -0,0 +1,17 @@
+package boj;
+import java.util.*;
+
+public class BOJ21598 { // SciComLove
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		for (int i = 0; i < N; i++) {
+			System.out.println("SciComLove");
+		}
+		
+		sc.close();
+	}
+}
