79e10c4974004f81580b5832960c590e92e4cdbd
jiyong <wldyd5689@gmail.com>
2022-08-03 21:06:27 +0900

    재귀함수가 뭔가요?

---
+++ STUDY/src/boj/BOJ17478.java
@@ -0,0 +1,34 @@
+package boj;
+import java.util.*;
+
+public class BOJ17478 { // 재귀함수가 뭔가요?
+	
+	static int N;
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt(); // 교수님이 출력을 원하는 재귀 횟수
+		
+		System.out.println("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.");
+		recursive(0, "");
+		sc.close();
+	}
+	
+	public static void recursive(int n, String str) {
+		
+		if (n == N) {
+			System.out.println(str + "\"재귀함수가 뭔가요?\"");
+            System.out.println(str + "\"재귀함수는 자기 자신을 호출하는 함수라네\"");
+            System.out.println(str + "라고 답변하였지.");
+            return;
+		}
+		
+		System.out.println(str + "\"재귀함수가 뭔가요?\"");
+        System.out.println(str + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.");
+        System.out.println(str + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
+        System.out.println(str + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
+        recursive(n + 1, str + "____");
+        System.out.println(str + "라고 답변하였지.");
+	}
+}
