822d151c347c006412d0e75d6c65b60e60be2a5c
jiyong <wldyd5689@gmail.com>
2022-08-04 10:38:09 +0900

    녹색거탑

---
+++ STUDY/src/boj/BOJ24723.java
@@ -0,0 +1,14 @@
+package boj;
+import java.util.*;
+
+public class BOJ24723 { // 녹색거탑
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		System.out.println((int) Math.pow(2, N));
+		sc.close();
+	}
+}
