cef9881db29ded42501aba07784d843b12685236
jiyong <wldyd5689@gmail.com>
2022-08-06 16:48:04 +0900

    별 찍기 - 12

---
+++ STUDY/src/boj/BOJ2522.java
@@ -0,0 +1,33 @@
+package boj;
+import java.util.*;
+
+public class BOJ2522 { // 별 찍기 - 12
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N - i - 1; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < i + 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		for (int i = N - 1; i > 0; i--) {
+			for (int j = 0; j < N - i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		sc.close();
+	}
+}
