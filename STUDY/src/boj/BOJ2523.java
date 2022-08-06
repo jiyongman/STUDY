9d8dd35e7deba20cdc3fac4bd2d2470e9bf4ac94
jiyong <wldyd5689@gmail.com>
2022-08-06 16:10:20 +0900

    별 찍기 - 13

---
+++ STUDY/src/boj/BOJ2523.java
@@ -0,0 +1,27 @@
+package boj;
+import java.util.*;
+
+public class BOJ2523 { // 별 찍기 - 13
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < i + 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		for (int i = N - 1; i > 0; i--) {
+			for (int j = 0; j < i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		sc.close();
+	}
+}
