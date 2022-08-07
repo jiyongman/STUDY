683bc4568f3641c3371d1f8ed587e2f670f2b257
jiyong <wldyd5689@gmail.com>
2022-08-07 16:02:26 +0900

    수학은 체육과목 입니다

---
+++ STUDY/src/boj/BOJ15894.java
@@ -0,0 +1,14 @@
+package boj;
+import java.util.*;
+
+public class BOJ15894 { // 수학은 체육과목 입니다
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		long n = sc.nextLong();
+		
+		System.out.println(4 * n);
+		sc.close();
+	}
+}
