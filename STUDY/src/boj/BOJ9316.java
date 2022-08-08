c475d91560d095a04748495a1b9c63fd4f3c1aeb
jiyong <wldyd5689@gmail.com>
2022-08-08 13:01:29 +0900

    Hello Judge

---
+++ STUDY/src/boj/BOJ9316.java
@@ -0,0 +1,16 @@
+package boj;
+import java.util.*;
+
+public class BOJ9316 { // Hello Judge
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		for (int i = 1; i <= N; i++) {
+			System.out.println("Hello World, Judge " + i + "!");
+		}
+		sc.close();
+	}
+}
