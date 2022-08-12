0754a2f9dbcd27da49dd6ad0435e435a91ab6bbf
jiyong <wldyd5689@gmail.com>
2022-08-12 12:21:58 +0900

    행운의 편지

---
+++ STUDY/src/boj/BOJ13311.java
@@ -0,0 +1,13 @@
+package boj;
+import java.util.*;
+
+public class BOJ13311 { // 행운의 편지
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println(-1);
+		sc.close();
+	}
+}
