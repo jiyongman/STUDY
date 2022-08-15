ecd814189bea9bc73ae2c06cff0a5be799bef8e3
jiyong <wldyd5689@gmail.com>
2022-08-15 20:12:45 +0900

    Speed Limit

---
+++ STUDY/src/boj/BOJ4635.java
@@ -0,0 +1,33 @@
+package boj;
+import java.util.*;
+
+public class BOJ4635 { // Speed Limit
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while (true) {
+			int n = sc.nextInt(); // one or more data sets
+			
+			if (n == -1) {
+				break;
+			}
+			
+			int sum = 0;
+			int t2 = 0;
+			
+			for (int i = 0; i < n; i++) {
+				int s = sc.nextInt(); // the speed in miles per hour
+				int t = sc.nextInt(); // the total elapsed time
+				
+				sum += s * (t - t2);
+				t2 = t;
+			}
+			
+			System.out.println(sum + " miles");
+		}
+		
+		sc.close();
+	}
+}
