36e020f4386e5a4eb9c5981dfb5ba2b942f88339
jiyong <wldyd5689@gmail.com>
2022-08-15 18:19:03 +0900

    골뱅이 찍기 - ㅌ

---
+++ STUDY/src/boj/BOJ23811.java
@@ -0,0 +1,48 @@
+package boj;
+import java.util.*;
+
+public class BOJ23811 { // 골뱅이 찍기 - ㅌ
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < 5 * N; j++) {
+				System.out.print("@");
+			}
+			System.out.println();
+		}
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print("@");
+			}
+			System.out.println();
+		}
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < 5 * N; j++) {
+				System.out.print("@");
+			}
+			System.out.println();
+		}
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print("@");
+			}
+			System.out.println();
+		}
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < 5 * N; j++) {
+				System.out.print("@");
+			}
+			System.out.println();
+		}
+		
+		sc.close();
+	}
+}
