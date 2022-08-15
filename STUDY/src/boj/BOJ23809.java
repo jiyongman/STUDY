4bcc7f259c1e56dea5fa3c882907b1573f4c009e
jiyong <wldyd5689@gmail.com>
2022-08-15 18:30:04 +0900

    골뱅이 찍기 - 돌아간 ㅈ

---
+++ STUDY/src/boj/BOJ23809.java
@@ -0,0 +1,72 @@
+package boj;
+import java.util.*;
+
+public class BOJ23809 { // 골뱅이 찍기 - 돌아간 ㅈ
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print("@");
+			}
+			for (int j = 0; j < 3 * N; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < N; j++) {
+				System.out.print("@");
+			}
+			System.out.println();
+		}
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print("@");
+			}
+			for (int j = 0; j < 2 * N; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < N; j++) {
+				System.out.print("@");
+			}
+			System.out.println();
+		}
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < 3 * N; j++) {
+				System.out.print("@");
+			}
+			System.out.println();
+		}
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print("@");
+			}
+			for (int j = 0; j < 2 * N; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < N; j++) {
+				System.out.print("@");
+			}
+			System.out.println();
+		}
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print("@");
+			}
+			for (int j = 0; j < 3 * N; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < N; j++) {
+				System.out.print("@");
+			}
+			System.out.println();
+		}
+		
+		sc.close();
+	}
+}
