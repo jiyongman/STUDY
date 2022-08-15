abdc9df3555c12cd6ce96fa8b199f59868d53260
jiyong <wldyd5689@gmail.com>
2022-08-15 13:47:21 +0900

    현대모비스와 함께하는 부품 관리

---
+++ STUDY/src/boj/BOJ24724.java
@@ -0,0 +1,19 @@
+package boj;
+import java.util.*;
+
+public class BOJ24724 { // 현대모비스와 함께하는 부품 관리
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt(); // 부품 관리 횟수
+		
+		for (int x = 1; x <= T; x++) {
+			
+			System.out.println("Material Management " + x);
+			System.out.println("Classification ---- End!");
+		}
+		
+		sc.close();
+	}
+}
