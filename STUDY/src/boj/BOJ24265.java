c02afc2283d5d1f34450cf12bcdca71f85a9381a
jiyong <wldyd5689@gmail.com>
2022-08-16 19:24:27 +0900

    알고리즘 수업 - 알고리즘의 수행 시간 4

---
+++ STUDY/src/boj/BOJ24265.java
@@ -0,0 +1,15 @@
+package boj;
+import java.util.*;
+
+public class BOJ24265 { // 알고리즘 수업 - 알고리즘의 수행 시간 4
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		long n = sc.nextLong();
+		
+		System.out.println((n - 1) * n / 2);
+		System.out.println(2);
+		sc.close();
+	}
+}
