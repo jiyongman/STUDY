a64a26479e2def919b8377917502ebd646e4998e
jiyong <wldyd5689@gmail.com>
2022-08-17 11:32:10 +0900

    More Dice

---
+++ STUDY/src/boj/BOJ9288.java
@@ -0,0 +1,25 @@
+package boj;
+import java.util.*;
+
+public class BOJ9288 { // More Dice
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt(); // the number of test cases
+		
+		for (int x = 1; x <= t; x++) {
+			int s = sc.nextInt();
+			
+			System.out.println("Case " + x + ":");
+			
+			for (int y = 1; y <= s / 2; y++) {
+				if (s - y <= 6) {
+					System.out.println("(" + y + "," + (s - y) + ")");
+				}
+			}
+		}
+		
+		sc.close();
+	}
+}
