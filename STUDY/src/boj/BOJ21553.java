eb6f5ad8a8c531a31fd9a4f9c4c8e7c7ab7f67fc
jiyong <wldyd5689@gmail.com>
2022-08-17 15:37:56 +0900

    암호 만들기

---
+++ STUDY/src/boj/BOJ21553.java
@@ -0,0 +1,16 @@
+package boj;
+import java.util.*;
+
+public class BOJ21553 { // 암호 만들기
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String A = sc.next(); // 문자열
+		String P = sc.next(); // 비밀번호
+		String B = P;
+		
+		System.out.println(B);
+		sc.close();
+	}
+}
