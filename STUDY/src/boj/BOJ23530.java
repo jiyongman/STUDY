ef98361525832acd5a7e91b6e4d0b0f61def1c2c
jiyong <wldyd5689@gmail.com>
2022-08-17 10:41:26 +0900

    Not A + B

---
+++ STUDY/src/boj/BOJ23530.java
@@ -0,0 +1,20 @@
+package boj;
+import java.util.*;
+
+public class BOJ23530 { // Not A + B
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt(); // the number of tests
+		
+		for (int i = 0; i < t; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			System.out.println(1);
+		}
+		
+		sc.close();
+	}
+}
