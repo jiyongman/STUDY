85e9742038feb580a5385129ff81370fe4eb4cab
jiyong <wldyd5689@gmail.com>
2022-08-18 12:13:31 +0900

    Math Tutoring

---
+++ STUDY/src/boj/BOJ9299.java
@@ -0,0 +1,33 @@
+package boj;
+import java.util.*;
+
+public class BOJ9299 { // Math Tutoring
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt(); // the number of test cases
+		
+		for (int x = 1; x <= t; x++) {
+			int n = sc.nextInt(); // the highest exponent of the polynomial
+			int[] a = new int[n + 1];
+			int k = n;
+			
+			System.out.print("Case " + x + ": " + (n - 1) + " ");
+			
+			for (int y = 0; y < n + 1; y++) {
+				a[y] = sc.nextInt();
+			}
+			
+			for (int y = 0; y < n; y++) {
+				a[y] *= k;
+				System.out.print(a[y] + " ");
+				k--;
+			}
+			
+			System.out.println();
+		}
+		
+		sc.close();
+	}
+}
