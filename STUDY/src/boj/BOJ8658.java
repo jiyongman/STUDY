95558ab1b5fe9558c72916453c826a6b69de0e0c
jiyong <wldyd5689@gmail.com>
2022-08-20 22:07:01 +0900

    Liczba

---
+++ STUDY/src/boj/BOJ8658.java
@@ -0,0 +1,27 @@
+package boj;
+import java.util.*;
+
+public class BOJ8658 { // Liczba
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for (int i = 1; i <= n; i++) {
+			if (n % i > 0) {
+				System.out.print(i + " ");
+				break;
+			}
+		}
+		
+		for (int i = n; i >= 1; i--) {
+			if (n % i > 0) {
+				System.out.print(i);
+				break;
+			}
+		}
+		
+		sc.close();
+	}
+}
