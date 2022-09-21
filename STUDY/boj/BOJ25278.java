25c2c6bc8bd9b04b00f1549210bb87edf833f09a
jiyongman <wldyd5689@gmail.com>
2022-09-21 17:57:01 +0900

    Terraforming

--- STUDY/boj/BOJ25278.java
+++ STUDY/boj/BOJ25278.java
@@ -7,29 +7,29 @@ public class BOJ25278 { // Terraforming
 
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(); // the number of environmental changes on Mars
-        int tem = -30;
-        int oce = 0;
-        int oxy = 0;
+        int temperature = -30;
+        int ocean = 0;
+        int oxygen = 0;
 
         for (int i = 0; i < n; i++) {
             String str = sc.next();
             int m = sc.nextInt();
 
             if (str.equals("temperature")) {
-                tem += m;
+                temperature += m;
             } else if (str.equals("ocean")) {
-                oce += m;
+                ocean += m;
             } else {
-                oxy += m;
+                oxygen += m;
             }
         }
 
-        if (tem >= 8 && oce >= 9 && oxy >= 14) {
+        if (temperature >= 8 && ocean >= 9 && oxygen >= 14) {
             System.out.println("liveable");
         } else {
             System.out.println("not liveable");
         }
-        
+
         sc.close();
     }   
 }
