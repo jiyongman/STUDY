45293fd7ba4c1af2dc9e6a75839ca4f4d2e42fcb
jiyongman <wldyd5689@gmail.com>
2022-09-25 22:18:28 +0900

    Dyslexia

---
+++ STUDY/boj/BOJ8371.java
@@ -0,0 +1,23 @@
+package boj;
+import java.util.*;
+
+public class BOJ8371 { // Dyslexia
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // the length of the original text
+        String str = sc.next();
+        String str2 = sc.next();
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (str.charAt(i) != str2.charAt(i)) {
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+        sc.close();
+    }
+}
