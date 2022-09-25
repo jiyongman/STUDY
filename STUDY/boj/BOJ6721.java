e05c1209814252c417f1553bd59cf53695dd367c
jiyongman <wldyd5689@gmail.com>
2022-09-25 19:19:45 +0900

    Backward numbers

---
+++ STUDY/boj/BOJ6721.java
@@ -0,0 +1,26 @@
+package boj;
+import java.util.*;
+
+public class BOJ6721 { // Backward numbers
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(); // the number of test cases
+
+        for (int i = 0; i < N; i++) {
+            String str = sc.next();
+            String str2 = sc.next();
+            str = new StringBuffer(str).reverse().toString();
+            str2 = new StringBuffer(str2).reverse().toString();
+            int sum = Integer.parseInt(str) + Integer.parseInt(str2);
+            String str3 = Integer.toString(sum);
+            str3 = new StringBuffer(str3).reverse().toString();
+            int result = Integer.parseInt(str3);
+
+            System.out.println(result);
+        }
+
+        sc.close();
+    }
+}
