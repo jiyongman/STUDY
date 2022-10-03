2343d702a1a3f5de9723a019e021cff3bbd7b056
jiyongman <wldyd5689@gmail.com>
2022-10-03 18:46:16 +0900

    그대로 출력하기 2

---
+++ STUDY/boj/BOJ11719.java
@@ -0,0 +1,17 @@
+package boj;
+import java.util.*;
+
+public class BOJ11719 { // 그대로 출력하기 2
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextLine()) {
+            String str = sc.nextLine();
+            System.out.println(str);
+        }
+
+        sc.close();
+    }
+}
