209bec2d29da03fa8794740361486adc57f089f0
jiyongman <wldyd5689@gmail.com>
2022-10-10 15:58:45 +0900

    Easy-to-Pronounce Words

--- STUDY/boj/BOJ25785.java
+++ STUDY/boj/BOJ25785.java
@@ -10,25 +10,13 @@ public class BOJ25785 { // Easy-to-Pronounce Words
         int cnt = 0;
 
         for (int i = 1; i < str.length(); i++) {
-            if (i % 2 == 0) {
-                if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u') {
-                    if (str.charAt(i - 1) != 'a' && str.charAt(i - 1) != 'e' && str.charAt(i - 1) != 'i' && str.charAt(i - 1) != 'o' && str.charAt(i - 1) != 'u') {
-                        cnt++;
-                    }
-                } else {
-                    if (str.charAt(i - 1) == 'a' || str.charAt(i - 1) == 'e' || str.charAt(i - 1) == 'i' || str.charAt(i - 1) == 'o' || str.charAt(i - 1) == 'u') {
-                        cnt++;
-                    }
+            if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u') {
+                if (str.charAt(i - 1) != 'a' && str.charAt(i - 1) != 'e' && str.charAt(i - 1) != 'i' && str.charAt(i - 1) != 'o' && str.charAt(i - 1) != 'u') {
+                    cnt++;
                 }
             } else {
-                if (str.charAt(i) != 'a' && str.charAt(i) != 'e' && str.charAt(i) != 'i' && str.charAt(i) != 'o' && str.charAt(i) != 'u') {
-                    if (str.charAt(i - 1) == 'a' || str.charAt(i - 1) == 'e' || str.charAt(i - 1) == 'i' || str.charAt(i - 1) == 'o' || str.charAt(i - 1) == 'u') {
-                        cnt++;
-                    }
-                } else {
-                    if (str.charAt(i - 1) != 'a' && str.charAt(i - 1) != 'e' && str.charAt(i - 1) != 'i' && str.charAt(i - 1) != 'o' && str.charAt(i - 1) != 'u') {
-                        cnt++;
-                    }
+                if (str.charAt(i - 1) == 'a' || str.charAt(i - 1) == 'e' || str.charAt(i - 1) == 'i' || str.charAt(i - 1) == 'o' || str.charAt(i - 1) == 'u') {
+                    cnt++;
                 }
             }
         }
