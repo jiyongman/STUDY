c59bd8ee65e7bf547301d44d4aa7edd9fb0d1366
jiyongman <wldyd5689@gmail.com>
2022-10-18 11:18:20 +0900

    접두사 배열

---
+++ STUDY/boj/BOJ13322.java
@@ -0,0 +1,17 @@
+package boj;
+import java.util.*;
+
+public class BOJ13322 { // 접두사 배열
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(i);
+        }
+
+        sc.close();
+    }
+}
