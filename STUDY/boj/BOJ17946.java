943b66ea09f97a77eb2bcecbc12ff99e9ada6715
jiyongman <wldyd5689@gmail.com>
2022-10-20 12:40:01 +0900

    피자는 나눌 수록 커지잖아요

---
+++ STUDY/boj/BOJ17946.java
@@ -0,0 +1,19 @@
+package boj;
+import java.util.*;
+
+public class BOJ17946 { // 피자는 나눌 수록 커지잖아요
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(); // 테스트 케이스의 수
+
+        for (int i = 0; i < N; i++) {
+            int K = sc.nextInt(); // 칼질의 최대 숫자
+
+            System.out.println(1);
+        }
+
+        sc.close();
+    }
+}
