78ceef48562a7be83d705d592025345864f31ceb
jiyongman <wldyd5689@gmail.com>
2022-10-23 18:18:33 +0900

    방어율 무시 계산하기

--- STUDY/boj/BOJ25756.java
+++ STUDY/boj/BOJ25756.java
@@ -6,11 +6,11 @@ public class BOJ25756 { // 방어율 무시 계산하기
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+        int N = sc.nextInt(); // 임스가 가지고 있는 물약의 개수
         double V = 0;
 
         for (int i = 0; i < N; i++) {
-            double A = sc.nextDouble();
+            double A = sc.nextDouble(); // 임스가 가지고 있는 물약으로 인해 증가되는 방어율 무시 수치
             V = 1 - (1 - V) * (1 - A / 100);
 
             System.out.println(V * 100);
