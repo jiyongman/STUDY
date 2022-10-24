761d780f16a4a49cb4d37d58099140c8d75f7105
jiyongman <wldyd5689@gmail.com>
2022-10-24 14:07:30 +0900

    초성 중성 종성

---
+++ STUDY/boj/BOJ11284.java
@@ -0,0 +1,20 @@
+package boj;
+import java.util.*;
+
+public class BOJ11284 { // 초성 중성 종성
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int n = str.charAt(0) - 0xAC00;
+        String[] arr = { "ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ" };
+        String[] arr2 = { "ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ", "ㅕ", "ㅖ", "ㅗ", "ㅘ", "ㅙ", "ㅚ", "ㅛ", "ㅜ", "ㅝ", "ㅞ", "ㅟ", "ㅠ", "ㅡ", "ㅢ", "ㅣ" };
+        String[] arr3 = { "", "ㄱ", "ㄲ", "ㄳ", "ㄴ", "ㄵ", "ㄶ", "ㄷ", "ㄹ", "ㄺ", "ㄻ", "ㄼ", "ㄽ", "ㄾ", "ㄿ", "ㅀ", "ㅁ", "ㅂ", "ㅄ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ" };
+
+        System.out.println(arr[n / 28 / 21]);
+        System.out.println(arr2[n / 28 % 21]);
+        System.out.println(arr3[n % 28]);
+        sc.close();
+    }
+}
