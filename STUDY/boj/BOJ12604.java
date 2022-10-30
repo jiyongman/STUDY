b8a05c296618f0d2b9c8e5c27adefa30ec0fa55e
jiyongman <wldyd5689@gmail.com>
2022-10-30 18:59:16 +0900

    Store Credit (Large)

---
+++ STUDY/boj/BOJ12604.java
@@ -0,0 +1,37 @@
+package boj;
+import java.util.*;
+
+public class BOJ12604 { // Store Credit (Large)
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(); // the number of test cases
+
+        for (int x = 1; x <= N; x++) {
+            int C = sc.nextInt(); // the amount of credit you have at the store
+            int I = sc.nextInt(); // the number of items in the store
+            int[] arr = new int[I + 1];
+            
+            for (int i = 1; i <= I; i++) {
+                arr[i] = sc.nextInt(); // the price of an item in the store
+            }
+
+            int a = 0;
+            int b = 0;
+
+            for (int i = 1; i <= I - 1; i++) {
+                for (int j = i + 1; j <= I; j++) {
+                    if (arr[i] + arr[j] == C) {
+                        a = i;
+                        b = j;
+                    }
+                }
+            }
+
+            System.out.printf("Case #%d: %d %d\n", x, a, b);
+        }
+
+        sc.close();
+    }
+}
