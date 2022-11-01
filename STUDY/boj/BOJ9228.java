e2bbd93dc0d05335817ed72be3b54e3276120042
jiyongman <wldyd5689@gmail.com>
2022-11-01 18:50:46 +0900

    Check Digits

---
+++ STUDY/boj/BOJ9228.java
@@ -0,0 +1,40 @@
+package boj;
+import java.util.*;
+
+public class BOJ9228 { // Check Digits
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String str = sc.next();
+
+            if (str.equals("#")) {
+                break;
+            }
+
+            int i = 2;
+            int tmp = 0;
+
+            for (int j = str.length() - 1; j >= 0; j--) {
+                tmp += (str.charAt(j) - '0') * i;
+                i++;
+            }
+
+            tmp %= 11;
+            tmp = 11 - tmp;
+            String str2 = "Rejected";
+
+            if (tmp < 10) {
+                System.out.printf("%s -> %d\n", str, tmp);
+            } else if (tmp == 10) {
+                System.out.printf("%s -> %s\n", str, str2);
+            } else if (tmp > 10) {
+                System.out.printf("%s -> %d\n", str, 0);
+            }
+        }
+
+        sc.close();
+    }
+}
