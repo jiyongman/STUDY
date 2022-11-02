f5088c429fb1ac64df441ca9c472f2bcc9864d1c
jiyongman <wldyd5689@gmail.com>
2022-11-02 18:04:22 +0900

    Celebrity jeopardy

---
+++ STUDY/boj/BOJ3765.java
@@ -0,0 +1,18 @@
+package boj;
+import java.util.*;
+
+public class BOJ3765 { // Celebrity jeopardy
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextLine()) {
+            String str = sc.nextLine();
+
+            System.out.println(str);
+        }
+
+        sc.close();
+    }
+}
