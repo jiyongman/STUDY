cfc478a2f4b657ad5f6a357a873f3822ecac0533
jiyongman <wldyd5689@gmail.com>
2022-12-16 21:34:59 +0900

    GBus count (Large)

---
+++ STUDY/boj/BOJ12185.java
@@ -0,0 +1,44 @@
+package boj;
+import java.util.*;
+
+public class BOJ12185 { // GBus count (Large)
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt(); // the number of test cases
+
+        for (int x = 1; x <= T; x++) {
+            int N = sc.nextInt(); // the number of GBuses
+            int[] A = new int[N];
+            int[] B = new int[N];
+
+            for (int i = 0; i < N; i++) {
+                A[i] = sc.nextInt();
+                B[i] = sc.nextInt();
+            }
+
+            int P = sc.nextInt(); // the number of cities
+            int[] C = new int[P];
+
+            System.out.printf("Case #%d: ", x);
+
+            for (int i = 0; i < P; i++) {
+                C[i] = sc.nextInt();
+                int cnt = 0;
+
+                for (int j = 0; j < N; j++) {
+                    if (C[i] >= A[j] && C[i] <= B[j]) {
+                        cnt++;
+                    }
+                }
+
+                System.out.printf("%d ", cnt);
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
