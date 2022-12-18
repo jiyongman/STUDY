e8da7eca56b10f87dfd5986cb1e55108d0f84158
jiyongman <wldyd5689@gmail.com>
2022-12-18 13:57:05 +0900

    Hook

---
+++ STUDY/boj/BOJ10189.java
@@ -0,0 +1,13 @@
+package boj;
+import java.util.*;
+
+public class BOJ10189 { // Hook
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.printf("#  # #### #### #  #\n#### #  # #  # # #\n#### #  # #  # # #\n#  # #### #### #  #");
+        sc.close();
+    }
+}
