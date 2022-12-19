ee8f3167babad9bcad23f764d0fdfdaedcd20ce0
jiyongman <wldyd5689@gmail.com>
2022-12-19 14:05:08 +0900

    Crowing

---
+++ STUDY/boj/BOJ2321.f90
@@ -0,0 +1,10 @@
+program MAIN
+
+    print '(a)', ',~'
+    print '(a)', '( 9> //)'
+    print '(a)', ' )(_///'
+    print '(a)', '( \_>/'
+    print '(a)', ' \__/'
+    print '(a)', '  ^^'
+    stop
+end
