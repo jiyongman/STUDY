702bc5d7cae419207aee56375b56f98186d5b973
jiyongman <wldyd5689@gmail.com>
2022-12-24 11:41:06 +0900

    Serca

---
+++ STUDY/boj/BOJ26766.java
@@ -0,0 +1,25 @@
+package boj;
+import java.util.*;
+
+public class BOJ26766 { // Serca
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(" @@@   @@@ ");
+            System.out.println("@   @ @   @");
+            System.out.println("@    @    @");
+            System.out.println("@         @");
+            System.out.println(" @       @ ");
+            System.out.println("  @     @  ");
+            System.out.println("   @   @   ");
+            System.out.println("    @ @    ");
+            System.out.println("     @     ");
+        }
+
+        sc.close();
+    }
+}
