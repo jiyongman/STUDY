beb03e5058f2ff891a77312bca3202effa330a3e
jiyongman <wldyd5689@gmail.com>
2023-01-01 21:58:25 +0900

    Sanic

---
+++ STUDY/boj/BOJ20737.java
@@ -0,0 +1,15 @@
+package boj;
+import java.util.*;
+
+public class BOJ20737 { // Sanic
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        double r = sc.nextDouble();
+        double x = r - 1;
+
+        System.out.println(x);
+        sc.close();
+    }
+}
