a6f130cd11c352b1027182db313115018554ce6c
jiyongman <wldyd5689@gmail.com>
2023-01-12 14:47:27 +0900

    좌표 정렬하기

---
+++ STUDY/boj/BOJ11650.java
@@ -0,0 +1,39 @@
+package boj;
+import java.io.*;
+import java.util.*;
+
+public class BOJ11650 { // 좌표 정렬하기
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int[][] coordinate = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            coordinate[i][0] = Integer.parseInt(st.nextToken());
+            coordinate[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(coordinate, (e1, e2) -> {
+            if (e1[0] == e2[0]) {
+                return e1[1] - e2[1];
+            } else {
+                return e1[0] - e2[0];
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            sb.append(String.format("%d %d\n", coordinate[i][0], coordinate[i][1]));
+        }
+
+        bw.write(String.valueOf(sb));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
