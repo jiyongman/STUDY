dece4bc664039b0df6af2e266855b916180c3e1a
jiyongman <wldyd5689@gmail.com>
2023-01-29 17:17:39 +0900

    時間 (Hour)

---
+++ STUDY/boj/BOJ27327.java
@@ -0,0 +1,14 @@
+package boj;
+import java.util.*;
+
+public class BOJ27327 { // 時間 (Hour)
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+
+        System.out.println(X * 24);
+        sc.close();
+    }
+}
