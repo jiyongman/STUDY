9af8a5cfed2677e870461c32f7a968b8e48d3110
jiyongman <wldyd5689@gmail.com>
2023-02-13 21:23:55 +0900

    소수가 아닌 수

---
+++ STUDY/boj/BOJ27465.java
@@ -0,0 +1,14 @@
+package boj;
+import java.util.*;
+
+public class BOJ27465 { // 소수가 아닌 수
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        System.out.println(1000000000);
+        sc.close();
+    }
+}
